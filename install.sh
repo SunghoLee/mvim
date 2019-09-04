9ff663694d721d5f99ed3b15b32f263c6b027ce6
SunghoLee <eshaj@kaist.ac.kr>
2019-09-04 16:44:05 +0900

    Support nvim and coq ide

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#for vim configuration
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
 vim +PluginInstall +qall
+
+#for nvim configuration
+mkdir -p ~/.config/nvim/bundle
+cp init.vim ~/.config/nvim/
+git clone https://framagit.org/tyreunom/coquille.git ~/.config/nvim/bundle
