d7299d9c3fc624e6701ee8f4d9f9c00e2d53a9d1
SunghoLee <eshaj@kaist.ac.kr>
2020-01-03 13:01:27 +0900

    For Coqtail plugin

--- init.vim
+++
@@ -1,8 +0,0 @@
-set runtimepath^=~/.vim runtimepath+=~/.vim/after runtimepath+=~/.config/nvim/bundle/coquille
-let &packpath = &runtimepath
-source ~/.vimrc
-
-au FileType coq call CoqLaunch()
-au FileType coq call coquille#FNMapping()
-
-let g:coquille_auto_move = 'true'
--- install.sh
+++ install.sh
@@ -2,11 +2,9 @@
 
 #for vim configuration
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#for vim-coq plugin
+git clone https://github.com/let-def/vimbufsync.git ~/.vim/pack/coq/start/vimbufsync
+#copy vim configuration file
 cp .vimrc ~/.vimrc
+#install plugins
 vim +PluginInstall +qall
-
-#for nvim configuration
-mkdir -p ~/.config/nvim/bundle
-cp init.vim ~/.config/nvim/
-git clone https://framagit.org/tyreunom/coquille.git ~/.config/nvim/bundle/coquille
-nvim +UpdateRemotePlugins +q
